aea377e66d7543ac2aaf9e522db5cc795c0bca34
Peter Rundqvist <svp95bos@hotmail.com>
2023-04-19 16:57:46 +0200

    started adding generator
    ok

---
+++ src/CommunityToolkit.Mvvm.SourceGenerators/ComponentModel/DTOMemberGenerator.cs
@@ -0,0 +1,45 @@
+// Peter Rundqvist, 2023
+using System.Collections.Immutable;
+using System.Linq;
+using CommunityToolkit.Mvvm.SourceGenerators.ComponentModel.Models;
+using CommunityToolkit.Mvvm.SourceGenerators.Extensions;
+using CommunityToolkit.Mvvm.SourceGenerators.Helpers;
+using CommunityToolkit.Mvvm.SourceGenerators.Models;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CommunityToolkit.Mvvm.SourceGenerators.ComponentModel;
+
+[Generator(LanguageNames.CSharp)]
+public sealed partial class DTOMemberGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // Gather info for all annotated fields
+        IncrementalValuesProvider<(HierarchyInfo Hierarchy, Result<PropertyInfo?> Info)> propertyInfoWithErrors =
+            context.SyntaxProvider
+            .ForAttributeWithMetadataName(
+                "CommunityToolkit.Mvvm.ComponentModel.DTOMemberAttribute",
+                static (node, _) => node is VariableDeclaratorSyntax { Parent: VariableDeclarationSyntax { Parent: FieldDeclarationSyntax { Parent: ClassDeclarationSyntax or RecordDeclarationSyntax, AttributeLists.Count: > 0 } } },
+                static (context, token) =>
+                {
+                    if (!context.SemanticModel.Compilation.HasLanguageVersionAtLeastEqualTo(LanguageVersion.CSharp8))
+                    {
+                        return default;
+                    }
+
+                    FieldDeclarationSyntax fieldDeclaration = (FieldDeclarationSyntax)context.TargetNode.Parent!.Parent!;
+                    IFieldSymbol fieldSymbol = (IFieldSymbol)context.TargetSymbol;
+
+                    // Get the hierarchy info for the target symbol, and try to gather the property info
+                    HierarchyInfo hierarchy = HierarchyInfo.From(fieldSymbol.ContainingType);
+
+                    _ = Execute.TryGetInfo(fieldDeclaration, fieldSymbol, context.SemanticModel, token, out PropertyInfo? propertyInfo, out ImmutableArray<DiagnosticInfo> diagnostics);
+
+                    return (Hierarchy: hierarchy, new Result<PropertyInfo?>(propertyInfo, diagnostics));
+                })
+            .Where(static item => item.Hierarchy is not null);
+    }
+}
+
