770a20b044ecfc91b38671dca29741616896c490
Peter Rundqvist <svp95bos@hotmail.com>
2023-04-19 17:17:47 +0200

    IsTargetTypeValid

--- src/CommunityToolkit.Mvvm.SourceGenerators/ComponentModel/DTOMemberGenerator.Execute.cs
+++ src/CommunityToolkit.Mvvm.SourceGenerators/ComponentModel/DTOMemberGenerator.Execute.cs
@@ -19,7 +19,7 @@ namespace CommunityToolkit.Mvvm.SourceGenerators;
 partial class DTOMemberGenerator
 {
     /// <summary>
-    /// A container for all the logic for <see cref="ObservablePropertyGenerator"/>.
+    /// A container for all the logic for <see cref="DTOMemberGenerator"/>.
     /// </summary>
     internal static class Execute
     {
@@ -41,7 +41,44 @@ partial class DTOMemberGenerator
             [NotNullWhen(true)] out PropertyInfo? propertyInfo,
             out ImmutableArray<DiagnosticInfo> diagnostics)
         {
+            using ImmutableArrayBuilder<DiagnosticInfo> builder = ImmutableArrayBuilder<DiagnosticInfo>.Rent();
 
+            // Validate the target type
+            if (!IsTargetTypeValid(fieldSymbol, out bool shouldAddToDTO))
+            {
+                builder.Add(
+                    InvalidContainingTypeForObservablePropertyFieldError,
+                    fieldSymbol,
+                    fieldSymbol.ContainingType,
+                    fieldSymbol.Name);
+
+                propertyInfo = null;
+                diagnostics = builder.ToImmutable();
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Validates the containing type for a given field being annotated.
+        /// </summary>
+        /// <param name="fieldSymbol">The input <see cref="IFieldSymbol"/> instance to process.</param>
+        /// <param name="shouldAddToDTO">Whether or not property should be added to a DTO.</param>
+        /// <returns>Whether or not the containing type for <paramref name="fieldSymbol"/> is valid.</returns>
+        private static bool IsTargetTypeValid(IFieldSymbol fieldSymbol, out bool shouldAddToDTO)
+        {
+            // The [ObservableProperty] attribute can only be used in types that are known to expose the necessary OnPropertyChanged and OnPropertyChanging methods.
+            // That means that the containing type for the field needs to match one of the following conditions:
+            //   - It inherits from ObservableObject (in which case it also implements INotifyPropertyChanging).
+            //   - It has the [DTO] attribute (on itself or any of its base types).
+            //   - It has the [INotifyPropertyChanged] attribute (on itself or any of its base types).
+            //bool isObservableObject = fieldSymbol.ContainingType.InheritsFromFullyQualifiedMetadataName("CommunityToolkit.Mvvm.ComponentModel.ObservableObject");
+            bool hasDTOAttribute = fieldSymbol.ContainingType.HasOrInheritsAttributeWithFullyQualifiedMetadataName("CommunityToolkit.Mvvm.ComponentModel.DTOAttribute");
+            //bool hasINotifyPropertyChangedAttribute = fieldSymbol.ContainingType.HasOrInheritsAttributeWithFullyQualifiedMetadataName("CommunityToolkit.Mvvm.ComponentModel.INotifyPropertyChangedAttribute");
+
+            shouldAddToDTO = hasDTOAttribute;
+
+            return hasDTOAttribute;
         }
     }
 }
